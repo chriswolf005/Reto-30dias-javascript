42328c1a303a4dc315553e39ce4f28d0ca2a63d8
Chris <christophersanchezwolf005@gmail.com>
2023-02-26 13:12:14 -0400

    Mi primer commit, primer ejecricio, provando

---
+++ src/retornaTipo.js
@@ -0,0 +1,34 @@
+/*
+En este desafío encontrarás una función llamada solution que recibe un parámetro llamado valor. Debes encontrar el tipo de dato del parámetro valor y retornarlo desde la función solution.
+
+Recuerda que el parámetro valor será distinto por cada distinta forma en que ejecutemos la función solution.
+
+Por ejemplo:
+
+Dados los siguientes llamados a la función solution:
+
+solution(1)
+solution("Dieguillo")
+solution(true)
+
+Debes obtener los siguientes resultados:
+
+"number"
+"string"
+"boolean"
+*/
+
+/* ¡Hola, Platzinauta!
+Por favor ignora la palabra export que agregamos antes
+de la función solution, la necesitamos p
+ara ejecutar las pruebas de tu código.
+Por ahora no te preocupes por eso,
+más adelante en tu ruta de aprendizaje a
+prenderás a profundidad cómo funciona.
+¡Mucha suerte en tu reto! */
+
+ function solution(valor) {
+    // Escribe tu código aquí 👈
+    return typeof valor
+  }
+  
