bfb97c21cb8fdfe784f4601f02ca2e07d6fc09e8
Chris <christophersanchezwolf005@gmail.com>
2023-04-13 14:01:08 -0400

    Se creo el archivo carrito de compras con su respectivo esqueleto de clases

---
+++ src/cart.js
@@ -0,0 +1,29 @@
+// Product class
+ class Product {
+    // No debes editar este archivo ❌
+    constructor(name, price, quantity) {
+      this.name = name;
+      this.price = price;
+      this.quantity = quantity;
+    }
+  
+    addToCart() {
+      throw new Error(
+        "La lógica de este método debe ser implementada por las clases hijas"
+      );
+    }
+  }
+
+
+
+ class Article {
+  // Tu código aquí 👈
+}
+
+ class Service {
+  // Tu código aquí 👈
+}
+
+ class Cart {
+  // Tu código aquí 👈
+}
