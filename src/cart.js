3f0bdf93312467f7ba6ec34aa8f6d53533beee0c
Chris <christophersanchezwolf005@gmail.com>
2023-04-18 14:06:25 -0400

    El ejercicio cart.js, esta terminado

--- src/cart.js
+++ src/cart.js
@@ -16,14 +16,59 @@
 
 
 
- class Article {
+ class Article extends Product {
   // Tu código aquí 👈
+  constructor(name, price, quantity){
+    super(name, price, quantity)
+  }
+  addToCart(){
+    console.log(`Agregando ${this.quantity} unidades del articulo ${this.name}`)
+  }
 }
 
- class Service {
+ class Service extends Product {
   // Tu código aquí 👈
+  constructor(name, price){
+    super(name, price)
+  }
+  addToCart(){
+    console.log(`Agregando el servicio ${this.name} al carrito`)
+  }
 }
+
 
  class Cart {
   // Tu código aquí 👈
+  constructor() {
+    this.products = [];
+  }
+  addProduct(product){
+    this.products.push(product);
+    product.addToCart();
+  }
+  deleteProduct(product){
+    index=this.products.indexOf(product)
+    if(index!==-1){
+      this.products.splice(index,1)
+    }
+  }
+  calculateTotal() {
+    let total = 0;
+    this.products.forEach((product) => {
+      total += product.price;
+    });
+    return total;
+  }
+  
+  getProducts(){
+    return this.products
+  }
 }
+
+const book = new Article("Libro", 100, 2);
+const course = new Service("Curso", 120);
+
+const cart = new Cart();
+cart.addProduct(book);
+cart.addProduct(course);
+console.log(cart.calculateTotal()); // debería imprimir 320
